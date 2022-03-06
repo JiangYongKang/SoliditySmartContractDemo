dc7c836e6e037f63e167f277eea711847918dd5b
JiangYongKang <vincent4502237@gmail.com>
2022-03-06 21:23:06 +0800

    添加 MerkleTree 合约验证

---
+++ contracts/MerkleTreeVerify.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+
+contract MerkleTreeVerify {
+    /**
+     * Merkle Root Hash Value
+     */
+    bytes32 public saleMerkleRoot;
+
+    constructor(bytes32 merkleRoot) {
+        saleMerkleRoot = merkleRoot;
+    }
+
+    /**
+     * 需要有白名单权限才能正常调用
+     */
+    function sendMessage(
+        bytes32[] calldata merkleProof,
+        string calldata message
+    )
+        public
+        view
+        isValidMerkleProof(merkleProof, saleMerkleRoot)
+        returns (string memory)
+    {
+        return message;
+    }
+
+    /**
+     * 设置 Merkle Root 的值
+     */
+    function setSaleMerkleRoot(bytes32 merkleRoot) external {
+        saleMerkleRoot = merkleRoot;
+    }
+
+    /**
+     * 白名单修饰符
+     */
+    modifier isValidMerkleProof(bytes32[] calldata merkleProof, bytes32 root) {
+        require(
+            MerkleProof.verify(
+                merkleProof,
+                root,
+                keccak256(abi.encodePacked(msg.sender))
+            ),
+            "MerkleTreeVerify: merkleProof Verification Failed"
+        );
+        _;
+    }
+}
